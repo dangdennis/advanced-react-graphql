2ce727bc7e2e5984d539008ae3deffa743cbd48f
Dennis Dang <dangggdennis@gmail.com>
2018-10-05 22:58:33 -0700

    Create server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middlware to handle cookies (JWT)
+// TODO use express middleware to populate current users
+
+server.start(
+    {   
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL
+        },
+        playground: '/playground',
+    },
+    data => {
+        console.log(`Server is now running on port http://localhost:${data.port}`);
+    }
+);
