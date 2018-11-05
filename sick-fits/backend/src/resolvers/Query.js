04b27513ded9e0c27b04f46a25e656ced4e36b99
Dennis Dang <dennis.dang@coxautoinc.com>
2018-11-05 15:13:22 -0800

    Merging partial pagination resolver

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,14 +1,15 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
-    // You can forward the query directly to Prisma if no middleware action is required
-    items: forwardTo('db'),
-    item: forwardTo('db')
-    // async items(parent, args, ctx, info) {
-    //     const items = await ctx.db.query.items();
+  // You can forward the query directly to Prisma if no middleware action is required
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
+  // async items(parent, args, ctx, info) {
+  //     const items = await ctx.db.query.items();
 
-    //     return items;
-    // }
+  //     return items;
+  // }
 };
 
 module.exports = Query;
